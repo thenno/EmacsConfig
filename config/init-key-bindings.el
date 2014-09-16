d16b3a29a641713313e131b4750aa1534a86b9ab
Max Zuev <greeneyedtramp@yandex.ru>
2014-09-16 15:54:51 +0400

    Add new hotkeys

--- config/init-key-bindings.el
+++ config/init-key-bindings.el
@@ -1,5 +1,7 @@
 (global-set-key (kbd "C-x g") 'goto-line)
 (global-set-key "\C-s" 'isearch-forward-regexp)
 (global-set-key "\C-r" 'isearch-backward-regexp)
+(global-set-key "\C-\M-r" 'replace-regexp)
+(global-set-key "\M-n" (lambda () (interactive) (progn (move-end-of-line 1) (newline-and-indent))))
 
 (provide 'init-key-bindings)
