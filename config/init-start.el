b1ebcde8d0a721fe2b7d942c3239ed37e3a8a3c7
Max Zuev <greeneyedtramp@yandex.ru>
2014-12-16 18:08:21 +0300

    New theme

--- config/init-start.el
+++ config/init-start.el
@@ -6,13 +6,23 @@
  '(initial-frame-alist (quote ((fullscreen . maximized)))))
 
 ; theme settings
-;(add-to-list 'custom-theme-load-path "~/.emacs.d/themes/emacs-color-theme-solarized")
+(add-to-list 'custom-theme-load-path "~/.emacs.d/themes/tomorrow-night-paradise-theme")
 (set-default-font "Ubuntu Mono 14")
-(load-theme 'wombat t)
+(load-theme 'tomorrow-night-paradise t)
 
 ; dicable bars
 (tool-bar-mode -1)
 ; (menu-bar-mode -1)
 (scroll-bar-mode -1)
+
+(custom-set-variables
+ '(ansi-term-color-vector [unspecified "#000000" "#d54e53" "#afd75f" "#e7c547" "#5f87d7" "#af87d7" "#5f87d7" "#dadada"])
+ '(custom-safe-themes (quote ("4aafea32abe07a9658d20aadcae066e9c7a53f8e3dfbd18d8fa0b26c24f9082c" default)))
+ '(fci-rule-character-color "#1c1c1c")
+ '(fci-rule-color "#1c1c1c")
+ '(ido-enable-tramp-completion nil)
+ '(initial-frame-alist (quote ((fullscreen . maximized)))))
+(custom-set-faces
+ )
 
 (provide 'init-start)
