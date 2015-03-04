f59852e9003328971244ae576e56c6b5d8e163a6
Max Zuev <greeneyedtramp@yandex.ru>
2015-03-05 02:18:07 +0300

    Add new pylint ignore and color theme for company-mode

--- config/init-start.el
+++ config/init-start.el
@@ -16,6 +16,28 @@
     (scroll-bar-mode -1)
     (tool-bar-mode -1))
 
+; company-mode settings
+(custom-set-faces
+ `(company-scrollbar-bg
+   ((t (:background "lightgray"))))
+ `(company-scrollbar-fg
+   ((t (:background "#5f5f5f"))))
+ '(company-preview
+   ((t (:foreground "darkgray" :underline t))))
+ '(company-preview-common
+   ((t (:inherit company-preview))))
+ '(company-tooltip
+   ((t (:background "lightgray" :foreground "black"))))
+ '(company-tooltip-selection
+   ((t (:background "#5f5f5f" :foreground "white"))))
+ '(company-tooltip-common
+   ((((type x)) (:inherit company-tooltip :weight bold))
+    (t (:inherit company-tooltip))))
+ '(company-tooltip-common-selection
+   ((((type x)) (:inherit company-tooltip-selection :weight bold))
+    (t (:inherit company-tooltip-selection)))))
+
+; custom settings
 (custom-set-variables
  '(ansi-term-color-vector [unspecified "#000000" "#d54e53" "#afd75f" "#e7c547" "#5f87d7" "#af87d7" "#5f87d7" "#dadada"])
  '(custom-safe-themes (quote ("4aafea32abe07a9658d20aadcae066e9c7a53f8e3dfbd18d8fa0b26c24f9082c" default)))
