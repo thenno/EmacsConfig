99386bc54d72537bde0449b72934eabab8ac36fa
Max Zuev <thenno@yandex-team.ru>
2015-07-06 22:19:11 +0300

    Fix flycheck config

--- config/init-flycheck.el
+++ config/init-flycheck.el
@@ -1,3 +1,6 @@
-(setq flycheck-check-syntax-automatically '(mode-enabled save new-line))
+(defun my-flycheck-mode-hook ()
+  (setq flycheck-check-syntax-automatically '(mode-enabled save new-line))
+  )
 
+(add-hook 'flycheck-mode-hook 'my-flycheck-mode-hook)
 (provide 'init-flycheck)
