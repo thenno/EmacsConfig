07421ac2b0e2eca4ebfc16575a2b470b7077d451
Max Zuev <thenno@yandex-team.ru>
2015-09-10 19:31:36 +0300

    Fix color for tabs

--- config/init-programming.el
+++ config/init-programming.el
@@ -1,5 +1,5 @@
 (defun init-fill-column ()
-  (setq whitespace-style '(face lines-tail tab-mark trailing))
+  (setq whitespace-style '(face lines-tail tabs tab-mark trailing))
   (setq whitespace-line-column 79)
   (whitespace-mode)
   )
